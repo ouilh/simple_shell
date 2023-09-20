b4b90b8fb90631590f818454b682fd2fd75ad567
yassine <118019022+ouilh@users.noreply.github.com>
2023-09-21 00:57:06 +0100

    Update main.c

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ int main(int argc, char **argv)
     (void)argc;
     (void)argv;
 
-    run_shell();
+    _loop();
 
     return 0;
 }
@@ -19,16 +19,16 @@ int main(int argc, char **argv)
 /**
  * run_shell - Executes the shell command loop.
  */
-void run_shell(void)
+void _loop(void)
 {
     char *input_line;
     char **command_args;
     int execution_status;
 
     do {
-        input_line = read_input_line();
-        command_args = split_input_line(input_line);
-        execution_status = execute_command(command_args);
+        input_line = read_theline();
+        command_args = split_theline(input_line);
+        execution_status = _execute(command_args);
 
         free(input_line);
         free(command_args);
