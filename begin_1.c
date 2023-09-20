c6644f661b404eff7d86a330a293863f0e8aa65f
yassine <118019022+ouilh@users.noreply.github.com>
2023-09-20 22:51:54 +0100

    Create begin_1.c

---
+++ begin_1.c
@@ -0,0 +1,45 @@
+#include "shell.h"
+
+/**
+ * _begin - Execute a command using fork and execve.
+ * @args: The arguments of the command.
+ *
+ * Return: Always 1.
+ */
+int _begin(char **args)
+{
+	pid_t pid;
+	int stat;
+	char *cmd;
+
+	pid = fork();
+
+	if (pid == 0)
+	{
+		if (args[0][0] == '/')
+			cmd = args[0];
+		else
+			cmd = get_thepath(args[0]);
+
+		if (cmd)
+		{
+			execve(cmd, args, environ);
+			perror("execve");
+		}
+		else
+			perror("Command not found");
+
+		exit(1);
+	}
+	else if (pid < 0)
+	{
+		perror("fork");
+	}
+	else
+	{
+		do {
+			waitpid(pid, &stat, WUNTRACED);
+		} while (!WIFEXITED(stat) && !WIFSIGNALED(stat));
+	}
+	return (1);
+}
