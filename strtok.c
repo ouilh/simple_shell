953b4caddbebc538e7466486c280e04ded0f91fa
yassine <118019022+ouilh@users.noreply.github.com>
2023-09-20 23:04:35 +0100

    Update strtok.c

--- strtok.c
+++ strtok.c
@@ -1,100 +1,100 @@
 #include "shell.h"
 
 /**
- * is_delimiter - Checks if a character is a delimiter.
- * @ch: The character to check.
- * @delim: The string of delimiters.
+ * is_character_in_delimiters - Checks if a character is in a set of delimiters.
+ * @character: The character to check.
+ * @delimiters: The string of delimiters.
  *
- * Return: 1 if the character is a delimiter, 0 otherwise.
+ * Return: 1 if the character is in the delimiters, 0 otherwise.
  */
-int is_delimiter(char ch, const char *delim)
+int is_character_in_delimiters(char character, const char *delimiters)
 {
-	size_t i;
+    size_t i;
 
-	for (i = 0; delim[i] != '\0'; i++)
-	{
-		if (ch == delim[i])
-		{
-			return (1);
-		}
-	}
-	return (0);
+    for (i = 0; delimiters[i] != '\0'; i++)
+    {
+        if (character == delimiters[i])
+        {
+            return 1;
+        }
+    }
+    return 0;
 }
 
 /**
- * find_next_token - Finds the next token in a string.
+ * find_next_non_delimiter - Finds the next non-delimiter character in a string.
  * @str: The string to search.
- * @delim: The string of delimiters.
+ * @delimiters: The string of delimiters.
+ *
+ * Return: Pointer to the next non-delimiter character if found, NULL otherwise.
+ */
+char *find_next_non_delimiter(char *str, const char *delimiters)
+{
+    int is_delimiter = 0;
+
+    while (*str != '\0')
+    {
+        is_delimiter = is_character_in_delimiters(*str, delimiters);
+
+        if (!is_delimiter)
+        {
+            return str;
+        }
+        str++;
+    }
+    return NULL;
+}
+
+/**
+ * find_next_delimiter - Finds the next delimiter character in a string.
+ * @str: The string to search.
+ * @delimiters: The string of delimiters.
+ *
+ * Return: Pointer to the next delimiter character if found, NULL otherwise.
+ */
+char *find_next_delimiter(char *str, const char *delimiters)
+{
+    int is_delimiter = 0;
+
+    while (*str != '\0')
+    {
+        is_delimiter = is_character_in_delimiters(*str, delimiters);
+
+        if (is_delimiter)
+        {
+            *str = '\0';
+            str++;
+            break;
+        }
+        str++;
+    }
+    return str;
+}
+
+/**
+ * custom_strtok - Tokenizes a string into a sequence of tokens.
+ * @input_string: The string to tokenize.
+ * @delimiters: The string of delimiters.
  *
  * Return: Pointer to the next token if found, NULL otherwise.
  */
-char *find_next_token(char *str, const char *delim)
+char *custom_strtok(char *input_string, const char *delimiters)
 {
-	int is_delim = 0;
+    static char *current_position;
+    char *token = NULL;
 
-	while (*str != '\0')
-	{
-		is_delim = is_delimiter(*str, delim);
+    if (input_string != NULL)
+    {
+        current_position = input_string;
+    }
 
-		if (!is_delim)
-		{
-			return (str);
-		}
-		str++;
-	}
-	return (NULL);
-}
+    if (current_position == NULL || *current_position == '\0')
+    {
+        return NULL;
+    }
 
-/**
- * find_next_delim - Finds the next delimiter in a string.
- * @str: The string to search.
- * @delim: The string of delimiters.
- *
- * Return: Pointer to the next delimiter if found, NULL otherwise.
- */
-char *find_next_delim(char *str, const char *delim)
-{
-	int is_delim = 0;
+    token = find_next_non_delimiter(current_position, delimiters);
+    current_position = find_next_delimiter(token, delimiters);
 
-	while (*str != '\0')
-	{
-		is_delim = is_delimiter(*str, delim);
-
-		if (is_delim)
-		{
-			*str = '\0';
-			str++;
-			break;
-		}
-		str++;
-	}
-	return (str);
-}
-
-/**
- * _strtok - Breaks a string into a sequence of tokens.
- * @thestring: The string to tokenize.
- * @delim: The string of delimiters.
- *
- * Return: Pointer to the next token if found, NULL otherwise.
- */
-char *_strtok(char *thestring, const char *delim)
-{
-	static char *strpos;
-	char *token = NULL;
-
-	if (thestring != NULL)
-	{
-		strpos = thestring;
-	}
-
-	if (strpos == NULL || *strpos == '\0')
-	{
-		return (NULL);
-	}
-
-	token = find_next_token(strpos, delim);
-	strpos = find_next_delim(token, delim);
-
-	return (token);
+    return token;
 }
