d343db1234599e203e7cb6c601d8dad91ddad603
yassine <118019022+ouilh@users.noreply.github.com>
2023-09-20 22:58:36 +0100

    Update builtin.c

--- builtin.c
+++ builtin.c
@@ -1,89 +1,90 @@
 #include "shell.h"
 
 /**
- * _cd - Change the current working directory.
+ * change_directory - Change the current working directory.
  * @args: The arguments of the command.
  *
  * Return: 1 on success, 0 on failure.
  */
-int _cd(char **args)
+int change_directory(char **args)
 {
-	if (chdir(args[1]) != 0)
-	{
-		perror("error occurred");
-	}
-	return (1);
+    if (chdir(args[1]) != 0)
+    {
+        perror("Directory change error");
+        return 0; // Return 0 on failure
+    }
+    return 1; // Return 1 on success
 }
 
 /**
- * _env - Print the current environment variables.
+ * print_environment - Print the current environment variables.
  *
  * Return: Always 1.
  */
-int _env(void)
+int print_environment(void)
 {
-	int e, v;
+    int env_index, char_index;
 
-	e = 0;
-	while (environ[e])
-	{
-		v = 0;
-		while (environ[e][v])
-		{
-			_putchar(environ[e][v]);
-			v++;
-		}
+    env_index = 0;
+    while (environ[env_index])
+    {
+        char_index = 0;
+        while (environ[env_index][char_index])
+        {
+            _putchar(environ[env_index][char_index]);
+            char_index++;
+        }
 
-		if (v != 0)
-			_putchar('\n');
-		e++;
-	}
-	return (1);
+        if (char_index != 0)
+            _putchar('\n');
+        env_index++;
+    }
+    return 1;
 }
 
 /**
- * _out - Placeholder function.
+ * placeholder_function - Placeholder function.
  * @args: The arguments of the command.
  *
  * Return: 0.
  */
-int _out(char **args)
+int placeholder_function(char **args)
 {
-	(void)args;
-	return (0);
+    (void)args; // Avoid unused variable warning
+    return 0;
 }
 
 /**
- * _execute - Execute a command.
+ * execute_command - Execute a command.
  * @args: The arguments of the command.
  *
  * Return: Result of the executed command.
  */
-int _execute(char **args)
+int execute_command(char **args)
 {
-	int i;
-	int count = 0;
+    int i;
+    int built_in_count = 0;
 
-	char *built_in_c[] = {
-		"cd",
-		"env",
-		"exit"
-	};
+    char *built_in_commands[] = {
+        "cd",
+        "env",
+        "exit"
+    };
 
-	int (*built_in_f[])() = {&_cd, &_env, &_out};
+    int (*built_in_functions[])() = {&change_directory, &print_environment, &placeholder_function};
 
-	count = (sizeof(built_in_c) / sizeof(char *));
+    built_in_count = sizeof(built_in_commands) / sizeof(char *);
 
-	if (args[0] == NULL)
-	{
-		return (1);
-	}
+    if (args[0] == NULL)
+    {
+        return 1; // No command provided
+    }
 
-	for (i = 0; i < count; i++)
-	{
-		if (_strcmp(args[0], built_in_c[i]) == 0)
-			return ((*built_in_f[i])(args));
-	}
+    for (i = 0; i < built_in_count; i++)
+    {
+        if (_strcmp(args[0], built_in_commands[i]) == 0)
+            return (*built_in_functions[i])(args); // Execute the built-in function
+    }
 
-	return (_begin(args));
+    return execute_external_command(args); // Execute an external command
 }
