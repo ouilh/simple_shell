62d3ec5ef72109fd015b835e157804688caa9dcb
yassine <118019022+ouilh@users.noreply.github.com>
2023-09-21 18:12:25 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,43 @@
+#include "shell.h"
+
+/**
+ * main - Entry point of the Shell program.
+ *@argc: Number of arguments passed.
+ *@argv: Array of arguments passed.
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+
+	loo();
+
+	return (0);
+}
+
+/**
+ * loo - Executes the shell command loop.
+ */
+void loop(void)
+{
+	char *line;
+	char **cmdarg;
+	int exe;
+
+	line = r_t();
+	cmdarg = s_t(line);
+	exe = _ex(cmdarg);
+
+	free(theline);
+	free(cmdarg);
+  while (exe);
+  {
+    line = r_t();
+		cmdarg = s_t(line);
+		exe = _ex(cmdarg);
+
+		free(theline);
+		free(cmdarg);
+  }
+}
