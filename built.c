42ca3359b7239babf406a4c7e5c71d105b9b9d69
yassine <118019022+ouilh@users.noreply.github.com>
2023-09-21 18:03:41 +0100

    Update built.c

--- built.c
+++ built.c
@@ -78,13 +78,13 @@ int _ex(char **args)
 	{
 		return (1);
 	}
-  j = 0;
-
-  while (j < count)
-  {	
-	if (_strcmp(args[0], built_in_c[j]) == 0)
-		return ((*built_in_f[j])(args));
-	j++;
-  }
-  return (_begin(args));
+	j = 0;
+	
+	while (j < count)
+  	{	
+		if (_strcmp(args[0], built_in_c[j]) == 0)
+			return ((*built_in_f[j])(args));
+		j++;
+ 	 }
+ 	 return (_begin(args));
 }
