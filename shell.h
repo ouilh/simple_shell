94bd09410e724921a826db43f62ec4872a5b9618
yassine <118019022+ouilh@users.noreply.github.com>
2023-09-21 20:39:02 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -13,13 +13,13 @@
 extern char **environ;
 
 /* Functions prototypes */
-char *_genv(const char *envar);
-int _ex(char **args);
-int beg(char **args);
-char *get_tp(char *cmd);
-char **s_t(char *theline);
-char *r_t(void);
-void loo(void);
+char *_getenv(const char *envar);
+int _execute(char **args);
+int _begin(char **args);
+char *get_thepath(char *cmd);
+char **split_theline(char *theline);
+char *read_theline(void);
+void _loop(void);
 
 /* Constants */
 #define MAX_BUFFER_SIZE 1024
@@ -28,27 +28,27 @@ void loo(void);
 ssize_t my_own_getline(char **lineptr, size_t *n, FILE *stream);
 
 /* Helper functions */
-ssize_t R_input(char *buffer, FILE *stream);
-int F_newline(char *buffer, int start, int end);
-void c_line(char *buffer, int start, int end, char *line);
+ssize_t read_input(char *buffer, FILE *stream);
+int find_newline(char *buffer, int start, int end);
+void copy_to_line(char *buffer, int start, int end, char *line);
 
 /* String manipulation functions */
-char *_strt(char *thestring, const char *delim);
-char *find_next_del(char *str, const char *delim);
-char *find_next_t(char *str, const char *delim);
-int is_del(char ch, const char *delim);
+char *_strtok(char *thestring, const char *delim);
+char *find_next_delim(char *str, const char *delim);
+char *find_next_token(char *str, const char *delim);
+int is_delimiter(char ch, const char *delim);
 
 /* String utility functions */
-int _putc(char m);
-int _strc(const char *str1, char *str2);
+int _putchar(char m);
+int _strcmp(const char *str1, char *str2);
 int _strlen(const char *str);
-char *_strca(char *destr, char *sourstr);
-char *_strcp(char *destr, char *sourstr);
+char *_strcat(char *destr, char *sourstr);
+char *_strcpy(char *destr, char *sourstr);
 
 /* Built-in shell commands */
-int _cd_(char **args);
-int _out_(char **args);
-int _env_(void);
+int _cd(char **args);
+int _out(char **args);
+int _env(void);
 
 
 #endif
