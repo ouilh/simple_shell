a6e1f1a0370accd8aff6923efebcf496040486b0
yassine <118019022+ouilh@users.noreply.github.com>
2023-09-21 20:37:46 +0100

    Update builtin.c

--- builtin.c
+++ builtin.c
@@ -1,12 +1,12 @@
 #include "shell.h"
 
 /**
- * _cd_ - Change the current working directory.
+ * _cd - Change the current working directory.
  * @args: The arguments of the command.
  *
  * Return: 1 on success, 0 on failure.
  */
-int _cd_(char **args)
+int _cd(char **args)
 {
 	if (chdir(args[1]) != 0)
 	{
@@ -16,52 +16,52 @@ int _cd_(char **args)
 }
 
 /**
- * _env_ - Print the current environment variables.
+ * _env - Print the current environment variables.
  *
  * Return: Always 1.
  */
-int _env_(void)
+int _env(void)
 {
-	int t, l;
+	int e, v;
 
-	t = 0;
-	while (environ[t])
+	e = 0;
+	while (environ[e])
 	{
-		l = 0;
-		while (environ[t][l])
+		v = 0;
+		while (environ[e][v])
 		{
-			_put(environ[t][l]);
+			_putchar(environ[e][v]);
 			v++;
 		}
 
 		if (v != 0)
-			_put('\n');
-		t++;
+			_putchar('\n');
+		e++;
 	}
 	return (1);
 }
 
 /**
- * _out_ - Placeholder function.
+ * _out - Placeholder function.
  * @args: The arguments of the command.
  *
  * Return: 0.
  */
-int _out_(char **args)
+int _out(char **args)
 {
 	(void)args;
 	return (0);
 }
 
 /**
- * _ex - Execute a command.
+ * _execute - Execute a command.
  * @args: The arguments of the command.
  *
  * Return: Result of the executed command.
  */
-int _ex(char **args)
+int _execute(char **args)
 {
-	int j;
+	int i;
 	int count = 0;
 
 	char *built_in_c[] = {
@@ -78,13 +78,12 @@ int _ex(char **args)
 	{
 		return (1);
 	}
-	j = 0;
 
-	while (j < count)
+	for (i = 0; i < count; i++)
 	{
-		if (_strc(args[0], built_in_c[j]) == 0)
-			return ((*built_in_f[j])(args));
-		j++;
+		if (_strcmp(args[0], built_in_c[i]) == 0)
+			return ((*built_in_f[i])(args));
 	}
-	return (_beg(args));
+
+	return (_begin(args));
 }
