c40790354bef7972bd231ac9d7361f5b1c166bf3
yassine <118019022+ouilh@users.noreply.github.com>
2023-10-18 22:34:44 +0100

    Update divide.c

--- divide.c
+++ divide.c
@@ -1,101 +1,104 @@
 #include "shell.h"
 
-void help_all(void);
-void help_alias(void);
-void help_cd(void);
-void help_exit(void);
-void help_help(void);
+int token_len(char *str, char *delim);
+int count_tokens(char *str, char *delim);
+char **_strtok(char *line, char *delim);
 
 /**
- * help_all - Displays all possible builtin shellby commands.
+ * token_len - Locates the delimiter index marking the end
+ * of the first token contained within a string.
+ * @str: The string to be searched.
+ * @delim: The delimiter character.
+ *
+ * Return: The delimiter index marking the end of
+ *         the intitial token pointed to be str.
  */
-void help_all(void)
+int token_len(char *str, char *delim)
 {
-	char *msg = "Shellby\nThese shell commands are defined internally.\n";
+	int index = 0, len = 0;
 
-	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = "Type 'help' to see this list.\nType 'help name' to find ";
-	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = "out more about the function 'name'.\n\n  alias   \t";
-	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = "alias [NAME[='VALUE'] ...]\n  cd    \tcd   ";
-	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = "[DIRECTORY]\n  exit    \texit [STATUS]\n  env     \tenv";
-	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = "\n  setenv  \tsetenv [VARIABLE] [VALUE]\n  unsetenv\t";
-	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = "unsetenv [VARIABLE]\n";
-	write(STDOUT_FILENO, msg, _strlen(msg));
+	while (*(str + index) && *(str + index) != *delim)
+	{
+		len++;
+		index++;
+	}
+
+	return (len);
 }
 
 /**
- * help_alias - Displays information on the shellby builtin command 'alias'.
+ * count_tokens - Counts the number of delimited
+ *                words contained within a string.
+ * @str: The string to be searched.
+ * @delim: The delimiter character.
+ *
+ * Return: The number of words contained within str.
  */
-void help_alias(void)
+int count_tokens(char *str, char *delim)
 {
-	char *msg = "alias: alias [NAME[='VALUE'] ...]\n\tHandles aliases.\n";
+	int index, tokens = 0, len = 0;
 
-	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = "\n\talias: Prints a list of all aliases, one per line, in ";
-	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = "the format NAME='VALUE'.\n\talias name [name2 ...]:prints";
-	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = " the aliases name, name2, etc. one per line, in the ";
-	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = "form NAME='VALUE'.\n\talias NAME='VALUE' [...]: Defines";
-	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = " an alias for each NAME whose VALUE is given. If NAME ";
-	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = "is already an alias, replace its value with VALUE.\n";
-	write(STDOUT_FILENO, msg, _strlen(msg));
+	for (index = 0; *(str + index); index++)
+		len++;
+
+	for (index = 0; index < len; index++)
+	{
+		if (*(str + index) != *delim)
+		{
+			tokens++;
+			index += token_len(str + index, delim);
+		}
+	}
+
+	return (tokens);
 }
 
 /**
- * help_cd - Displays information on the shellby builtin command 'cd'.
+ * _strtok - Tokenizes a string.
+ * @line: The string.
+ * @delim: The delimiter character to tokenize the string by.
+ *
+ * Return: A pointer to an array containing the tokenized words.
  */
-void help_cd(void)
+char **_strtok(char *line, char *delim)
 {
-	char *msg = "cd: cd [DIRECTORY]\n\tChanges the current directory of the";
+	char **ptr;
+	int index = 0, tokens, t, letters, l;
 
-	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = " process to DIRECTORY.\n\n\tIf no argument is given, the ";
-	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = "command is interpreted as cd $HOME. If the argument '-' is";
-	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = " given, the command is interpreted as cd $OLDPWD.\n\n";
-	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = "\tThe environment variables PWD and OLDPWD are updated ";
-	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = "after a change of directory.\n";
-	write(STDOUT_FILENO, msg, _strlen(msg));
-}
+	tokens = count_tokens(line, delim);
+	if (tokens == 0)
+		return (NULL);
 
-/**
- * help_exit - Displays information on the shellby builtin command 'exit'.
- */
-void help_exit(void)
-{
-	char *msg = "exit: exit [STATUS]\n\tExits the shell.\n\n\tThe ";
+	ptr = malloc(sizeof(char *) * (tokens + 2));
+	if (!ptr)
+		return (NULL);
 
-	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = "STATUS argument is the integer used to exit the shell.";
-	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = " If no argument is given, the command is interpreted as";
-	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = " exit 0.\n";
-	write(STDOUT_FILENO, msg, _strlen(msg));
-}
+	for (t = 0; t < tokens; t++)
+	{
+		while (line[index] == *delim)
+			index++;
 
-/**
- * help_help - Displays information on the shellby builtin command 'help'.
- */
-void help_help(void)
-{
-	char *msg = "help: help\n\tSee all possible Shellby builtin commands.\n";
+		letters = token_len(line + index, delim);
 
-	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = "\n      help [BUILTIN NAME]\n\tSee specific information on each ";
-	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = "builtin command.\n";
-	write(STDOUT_FILENO, msg, _strlen(msg));
+		ptr[t] = malloc(sizeof(char) * (letters + 1));
+		if (!ptr[t])
+		{
+			for (index -= 1; index >= 0; index--)
+				free(ptr[index]);
+			free(ptr);
+			return (NULL);
+		}
+
+		for (l = 0; l < letters; l++)
+		{
+			ptr[t][l] = line[index];
+			index++;
+		}
+
+		ptr[t][l] = '\0';
+	}
+	ptr[t] = NULL;
+	ptr[t + 1] = NULL;
+
+	return (ptr);
 }
